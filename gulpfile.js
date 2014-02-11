01d99feb2ab25b219c66bed371d0dcdfa0a4f777
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-02-11 00:53:02 +0000

    Resolve #2 by implementing gulp-autoprefixer and removing mixins that are only used for browser prefixes.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp   = require('gulp'),
     sass   = require('gulp-sass'),
     cssmin = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     header = require('gulp-header'),
     pkg    = require('./package.json'),
@@ -17,6 +18,7 @@ var gulp   = require('gulp'),
 gulp.task('build', function() {
   return gulp.src('src/*.scss')
     .pipe( sass() )
+    .pipe( prefix('> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1') )
     .pipe( header(banner, {pkg: pkg}) )
     .pipe( gulp.dest('dist/') )
     .pipe( cssmin() )
